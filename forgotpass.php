91a36513aa62aaedc01a325f1ecf08bb7a429dfb
Herve176 <hdjomo1@gmail.com>
2023-08-17 22:00:12 +0200

    modification on forgot page

--- forgotpass.php
+++ forgotpass.php
@@ -6,10 +6,14 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="controller.php" method="POST">
-<input type="hidden" name="forgotpass"><br>
-<label for="email" ></label>
+<form action="controller.php" method="POST">
 
+<input type="hidden" name="register" value="register-property">
+<label for="email">Email:</label><br>
+<input type="text" id="email" name="email"><br>
+<label for="password">Password:</label><br>
+<input type="password" id="password" name="password"><br>
+<input type="submit" value="Register">
 
     </form>
 </body>
