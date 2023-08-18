34da295cba1852e3aa72a94213d2caa4767e4964
Herve176 <hdjomo1@gmail.com>
2023-08-18 09:53:05 +0200

    made controller

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="controller.php" method="POST">
+
+<input type="hidden" name="login" value="login-property">
+<label for="email">Email:</label><br>
+<input type="text" id="email" name="email"><br>
+<label for="password">Password:</label><br>
+<input type="password" id="password" name="password"><br>
+<input type="submit" value="Login">
+
+    </form>
 </body>
 </html>
