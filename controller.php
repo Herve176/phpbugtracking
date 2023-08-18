862a965a0909835673ecb4416c197d5748678f96
Herve176 <hdjomo1@gmail.com>
2023-08-18 21:05:26 +0200

    controller

--- controller.php
+++ controller.php
@@ -1,20 +1,18 @@
 <?php
 include'connection.php';
 
-function collectpost($value=null)
-{
+
         $value=$_POST['post-action'];
 if ($value == 'register-property') {
 
     try {
         // Check if the POST value is set, otherwise use a default value
-        $value = isset($_POST['post-action']) ? $_POST['post-action'] : $value;
+        $id = '';
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $id = '';
             $connection = DB::getConnection();
-            $query = $connection->prepare("INSERT INTO account(id,email, password) VALUES (?,?,?)");
+            $query = $connection->prepare("INSERT INTO account (id,email, password) VALUES (?,?,?)");
 
             // Bind the values to the prepare statement
             $query->bindParam(1,$id);
@@ -29,7 +27,7 @@ if ($value == 'register-property') {
                 echo "Error: " . $e->getMessage();
     } 
     }
-}
+
 
 
 
