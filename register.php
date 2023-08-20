fd3fe694df6c2b1cb637e619a36143670a76ee59
Herve176 <hdjomo1@gmail.com>
2023-08-20 14:05:38 +0200

    added confirmation field to form

--- register.php
+++ register.php
@@ -9,10 +9,14 @@
 
 <form action="controller.php" method="POST">
 <input type="hidden" name="post-action" value="register-property">
+<label for="name">Name:</label><br>
+<input type="text" id="name" name="name"><br>
 <label for="email">Email:</label><br>
 <input type="text" id="email" name="email"><br>
 <label for="password">Password:</label><br>
 <input type="password" id="password" name="password"><br>
+<label for="confirmation-password">Confirm-password:</label><br>
+<input type="text" id="confirmation-password" name="confirmation-password"><br>
 <input type="submit" value="Register">
 
     </form>
