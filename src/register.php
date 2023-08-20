d37828e1b39a2bb51bd657b79f8428baa2ffbeea
Herve176 <hdjomo1@gmail.com>
2023-08-20 15:05:02 +0200

    added bootstrap

--- src/register.php
+++ src/register.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="index.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <form action="controller.php" method="POST">
-
-<input type="hidden" name="login" value="login-property">
+<div>
+<form action="controller.php" method="POST">
+<input type="hidden" name="post-action" value="register-property">
+<label for="name">Name:</label><br>
+<input type="text" id="name" name="name"><br>
 <label for="email">Email:</label><br>
 <input type="text" id="email" name="email"><br>
 <label for="password">Password:</label><br>
 <input type="password" id="password" name="password"><br>
-<input type="submit" value="Login">
+<label for="confirmation-password">Confirm-password:</label><br>
+<input type="text" id="confirmation-password" name="confirmation-password"><br>
+<input type="submit" value="Register">
 
     </form>
+    </div>
 </body>
 </html>
